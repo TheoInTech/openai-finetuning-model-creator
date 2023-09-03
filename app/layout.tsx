1f8ed020efe946873503eff3eebd6a8b8153e829
Theo Roque <theoroque95@gmail.com>
2023-09-03 15:26:41 +0800

    Update metadata and added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/toaster";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -9,6 +10,20 @@ export const metadata: Metadata = {
   title: "OpenAI Opensource Fine-tuning Model Creator",
   description:
     "An opensource tool to build and deploy your own OpenAI fine-tuning model.",
+  keywords: [
+    "openai",
+    "openai fine-tuning",
+    "openai fine-tuning model",
+    "openai fine-tuning model creator",
+    "openai fine-tuning model builder",
+    "openai fine-tuning model deployer",
+    "openai api",
+    "how to deploy openai fine-tuning model",
+    "how to build openai fine-tuning model",
+    "how to create openai fine-tuning model",
+    "how to train openai fine-tuning model",
+    "how to fine-tune openai fine-tuning model",
+  ],
 };
 
 export default function RootLayout({
@@ -22,6 +37,7 @@ export default function RootLayout({
         <main className="w-full h-full min-h-screen flex flex-col items-center">
           {children}
           <Toaster />
+          <Analytics />
         </main>
       </body>
     </html>
